4fd736bb15d6d749f168e4773bf52b57481f8798
walfud <hi@walfud.com>
2017-03-08 11:06:10 +0800

    header basic show and hide

--- app/src/main/java/com/walfud/pulllayoutdemo/MainActivity.java
+++ app/src/main/java/com/walfud/pulllayoutdemo/MainActivity.java
@@ -2,12 +2,53 @@ package com.walfud.pulllayoutdemo;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.walfud.pulllayout.PullLayout;
 
 public class MainActivity extends Activity {
+
+    private PullLayout mPl;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mPl = (PullLayout) findViewById(R.id.pl);
+
+        final View header = LayoutInflater.from(this).inflate(R.layout.header_pulllayout, mPl, false);
+        mPl.setHeader(new HeaderViewHolder(header));
+        mPl.setOnEventListener(new PullLayout.OnPullDownListener<HeaderViewHolder>() {
+            @Override
+            public void onPullDown(HeaderViewHolder headerViewHolder, int dy, double percent) {
+                ImageView iv = headerViewHolder.iv;
+                TextView tv = headerViewHolder.tv;
+
+                iv.setRotation((float) (percent * 360));
+                tv.setText(String.format("onPullDown: dy=%d, percent=%.2f", dy, percent));
+
+                mPl.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        mPl.hideHeader();
+                    }
+                }, 3 * 1000);
+            }
+        });
+    }
+
+    public static class HeaderViewHolder extends PullLayout.ViewHolder {
+
+        public ImageView iv;
+        public TextView tv;
+
+        public HeaderViewHolder(View view) {
+            super(view);
+            iv = (ImageView) view.findViewById(R.id.iv);
+            tv = (TextView) view.findViewById(R.id.tv);
+        }
     }
 }
--- pulllayout/src/main/java/com/walfud/pulllayout/PullLayout.java
+++ pulllayout/src/main/java/com/walfud/pulllayout/PullLayout.java
@@ -2,6 +2,8 @@ package com.walfud.pulllayout;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -13,40 +15,118 @@ public class PullLayout extends ViewGroup {
 
     public static final String TAG = "PullLayout";
 
-    private View mHeader;
-    private OnEventListener mOnEventListener;
+    private ViewHolder mHeaderViewHolder;
+    private OnPullDownListener mOnPullDownListener;
 
     public PullLayout(Context context, AttributeSet attrs) {
         super(context, attrs);
     }
 
     @Override
-    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        measureChildren(widthMeasureSpec, heightMeasureSpec);
+    }
 
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+        for (int i = 0; i < getChildCount(); i++) {
+            View child = getChildAt(i);
+
+            if (child == mHeaderViewHolder.view) {
+                child.layout(0, -child.getMeasuredHeight(), getMeasuredWidth(), 0);
+            } else {
+                child.layout(0, 0, getMeasuredWidth(), getMeasuredHeight());
+            }
+        }
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        switch (ev.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                return true;
+            case MotionEvent.ACTION_MOVE: {
+
+                return true;
+            }
+            default:
+                return false;
+        }
+    }
+
+    private int mDownY;
+    private boolean mEdgeY;
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        int y = (int) event.getY();
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                mDownY = y;
+                mEdgeY = false;
+                return true;
+            case MotionEvent.ACTION_MOVE: {
+                int dy = y - mDownY;
+                if (0 < dy) {
+                    if (mHeaderViewHolder != null) {
+                        int headerHeight = mHeaderViewHolder.view.getHeight();
+                        double percentY = dy / (double) headerHeight;
+                        if (dy < mHeaderViewHolder.view.getHeight()) {
+                            scrollTo(0, -dy);
+                            mOnPullDownListener.onPullDown(mHeaderViewHolder, dy, percentY);
+
+                            Log.e(TAG, String.format("onTouchEvent: ACTION_MOVE y=%d, dy=%d, percent=%.2f, headerHeight=%d", y, dy, percentY, headerHeight));
+                        } else {
+                            if (!mEdgeY) {
+                                mEdgeY = true;
+
+                                scrollTo(0, -dy);
+                                mOnPullDownListener.onPullDown(mHeaderViewHolder, headerHeight, 1.0);
+
+                                Log.e(TAG, String.format("onTouchEvent: ACTION_MOVE EDGE y=%d, dy=%d, percent=%.2f, headerHeight=%d", y, dy, 1.0, headerHeight));
+                            }
+                        }
+                    }
+                }
+                Log.i(TAG, String.format("onTouchEvent: ACTION_MOVE y=%d, dy=%d", y, dy));
+                return true;
+            }
+            default:
+                return false;
+        }
     }
 
     // Function
-    public PullLayout setHeader(View header) {
-        mHeader = header;
+    public <H extends ViewHolder> PullLayout setHeader(H header) {
+        removeView(header.view);
+        addView(header.view);
+        mHeaderViewHolder = header;
         return this;
     }
-    public PullLayout setOnEventListener(OnEventListener listener) {
-        mOnEventListener = listener;
+    public PullLayout setOnEventListener(OnPullDownListener listener) {
+        mOnPullDownListener = listener;
+        return this;
+    }
+
+    public PullLayout hideHeader() {
+        scrollTo(0, 0);
+
         return this;
     }
 
     //
-    public static class Config {
-        public double maxPullablePercent;
-        public double effectPercent;
-    }
-    public interface OnEventListener {
-        void onPullDown(int dy, double percent);
-    }
-    public static class SimpleOnEventListener implements OnEventListener {
-        @Override
-        public void onPullDown(int dy, double percent) {
+//    public static class Config {
+//        public double maxPullablePercent;
+//        public double effectPercent;
+//    }
+    public static abstract class ViewHolder {
+        public View view;
 
+        public ViewHolder(View view) {
+            this.view = view;
         }
+    }
+    public interface OnPullDownListener<T extends ViewHolder> {
+        void onPullDown(T headerViewHolder, int dy, double percent);
     }
 }
